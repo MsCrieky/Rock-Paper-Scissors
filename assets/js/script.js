fec314eecd30fca7da56bb48670b8e0347c9cf36
MsCrieky <>
2023-10-27 11:54:49 +0000

    Add the result function to the game in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 const possibleChoices = document.querySelectorAll("button");
 const playerAnswer = document.getElementById("user-choice");
 const aiAnswer = document.getElementById("computer-choice");
+const resultView = document.getElementById("result");
 let userPick;
 let aiPick;
+let result;
 
 //Makes the button listen for clicks and put an event to it
 possibleChoices.forEach(button => button.addEventListener('click', (e) => {
     userPick = e.target.id
     playerAnswer.innerHTML = userPick
     generateComputerChoice()
-
+    getResult()
 }))
 
 //Creates the computer answer
@@ -30,4 +32,20 @@ function generateComputerChoice() {
             aiPick = " "; //Default, incase someting wierd happens
     }
     aiAnswer.innerHTML = aiPick;
+}
+
+function getResult() {
+    if(aiPick === userPick) {
+        result = "Its a Draw... Play it again Sam!";
+    } else if (
+        (aiPick === "ROCK" && userPick === "SCISSORS") ||
+        (aiPick === "PAPER" && userPick === "ROCK") ||
+        (aiPick === "SCISSORS" && userPick === "PAPER")
+    ) {
+        result = "Oh No... You Lose!";
+    } else {
+        result = "Yeah, You Win!"
+    }
+    resultView.innerHTML = result;
+
 }
